bc2bc41371a40dfc5e7f2e2152e8a076e1a49399
Siraj Hassni <siraajhassni@gmail.com>
2020-01-09 22:30:23 +0500

    upload

---
+++ includes/navbar.php
@@ -0,0 +1,29 @@
+ <html>
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+ <head>
+     <title></title>
+     <?php include_once 'includes/links.php'; ?>
+ </head>
+
+ <body>
+     <div class="header">
+         <div class="logo">
+             <a href="#">School Ms</a>
+         </div>
+         <nav>
+
+             <ul>
+                 <li><a href="#">Home</a></li>
+                 <li><a href="#">About Us</a></li>
+                 <li><a href="#">Contect</a></li>
+                 <li><a href="#">Subjects</a></li>
+                 <li><a href="#">Fee structure</a></li>
+             </ul>
+         </nav>
+         <div class="login-side">
+             <a href="#">Login</a>
+         </div>
+
+     </div>
