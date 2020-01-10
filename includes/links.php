5ed6382f397df195d004dde0fa594aa762e23901
Siraj Hassni <siraajhassni@gmail.com>
2020-01-10 23:41:52 +0500

    ad font awesomoe cdn

--- includes/links.php
+++ includes/links.php
@@ -1,7 +1,8 @@
 <!-- css links -->
 <link rel="stylesheet" href="source/css/app.css">
 <link rel="stylesheet" href="foundation.css">
-
+<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
+    integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 
 
 
