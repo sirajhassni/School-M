5af95a7d0fe7406b160cc6408e3ea24849afdce2
Nomanalizainozai <Nomanalizainozai@gmail.com>
2020-01-11 01:47:06 +0500

    Slider Task Completed

--- includes/links.php
+++ includes/links.php
@@ -1,13 +1,17 @@
 <!-- css links -->
 <link rel="stylesheet" href="source/css/app.css">
-<link rel="stylesheet" href="foundation.css">
-
+<link rel="stylesheet" href="source/css/foundation.css">
+<link rel="stylesheet" href="source/css/slick_custom.css">
+<link rel="stylesheet" href="source/css/slick.css">
+<link rel="stylesheet" href="source/css/slick-theme.css">
 
 
 
 <!-- js links -->
-
-<script src="js/vendor/jquery.js"></script>
-<script src="js/vendor/what-input.js"></script>
-<script src="js/vendor/foundation.js"></script>
-<script src="js/app.js"></script>
+<script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
+<script src="source/js/slick_custom.js"></script>
+<script src="source/js/slick.js"></script>
+<!-- <script src="source/js/jquery.js"></script> -->
+<script src="source/js/what-input.js"></script>
+<script src="source/js/foundation.js"></script>
+<!-- <script src="js/app.js"></script> -->
