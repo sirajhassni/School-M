bc557e4e5cfdeee5b7c8e4877233a3e4c77140c2
Siraj Hassni <siraajhassni@gmail.com>
2020-01-10 22:29:38 +0500

    clear all css histry and banner img

--- index.php
+++ index.php
@@ -3,9 +3,8 @@
 include_once 'includes/navbar.php';
 ?>
 <div class=" banner">
-    <img class="show-for-small-only" src="imgs/banner-2.jpg" alt="">
+
 </div>
 
 <?php
 include_once 'includes/footer.php';
-?>
