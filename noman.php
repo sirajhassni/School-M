12eee12d696848e6e7af3d69f914e795083b98a5
Nomanalizainozai <Nomanalizainozai@gmail.com>
2020-01-10 16:50:06 +0500

    Just For Practice

---
+++ noman.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+  <form method="POST">
+<input type="text" name="num"/>
+<input type="submit" name="sub"/>
+</form>
+<table class="table">
+    <tbody>
+<?php
+if(isset($_POST['sub'])){
+$n=$_POST['num'];
+    for($i=1;$i<=10;$i++){
+?>
+ <tr>
+            <td><?php echo $n;?></td>
+            <td><?php echo "X";?></td>
+            <td><?php echo $i;?></td>
+            <td><?php echo "=";?></td>
+            <td><?php echo $i*$n;?></td>
+        </tr>
+<?php   
+    }
+}
+?>
+</tbody>
+</table>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+
