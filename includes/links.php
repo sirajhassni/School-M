9a0433c21b764fe9e319bce3092e65ef8a3c8805
Siraj Hassni <siraajhassni@gmail.com>
2020-01-12 22:47:31 +0500

    adding font aweseome

--- includes/links.php
+++ includes/links.php
@@ -1,14 +1,16 @@
 <!-- css links -->
 <link rel="stylesheet" href="source/css/app.css">
-<link rel="stylesheet" href="foundation.css">
-<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
-    integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+<link rel="stylesheet" href="source/foundation.css">
+<link rel="stylesheet" href="source/font-awesome/css/all.css">  
+<link rel="stylesheet" href="source/font-awesome/css/all.min.css">  
 
 
 
 <!-- js links -->
 
-<script src="js/jquery.js"></script>
-<script src="js/what-input.js"></script>
-<script src="js/foundation.js"></script>
-<script src="js/app.js"></script>
+<script src="source/js/jquery.js"></script>
+<script src="source/js/what-input.js"></script>
+<script src="source/js/foundation.js"></script>
+<script src="source/js/app.js"></script>
+<!-- <script src="source/font-awesome/js/all.js"></script>
+<script src="source/font-awesome/js/all.min.js"></script> -->
--- includes/navbar.php
+++ includes/navbar.php
@@ -9,13 +9,25 @@
 
  <body>
 
-     <div class="header small-12">
+     <div class="header small-12 meduim-8">
 
          <div class="top-bar">
              <ul class="contects-info">
-                 <li>Contect</li>
+
+                 <li><i class="fa fa-phone"></i>033333544</li>
+                 <li> <a href="#"><i class="fa fa-envelope"></i>Teamalpha159k@gmail.com</a></li>
+                 <li><i class="fa fa-hourglass"></i>9:00 AM to 11:00 AM</li>
 
              </ul>
+
+             <div class="social-icons">
+                 <ul>
+                     <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                     <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+                     <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                     <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                 </ul>
+             </div>
 
          </div>
      </div>
