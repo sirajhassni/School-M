1085c0184b9679ce5d222246c3d73696e4d9574d
Siraj Hassni <siraajhassni@gmail.com>
2020-01-14 11:34:23 +0500

    workig on header

--- includes/navbar.php
+++ includes/navbar.php
@@ -12,14 +12,16 @@
      <div class="header small-12 meduim-8">
 
          <div class="top-bar">
-             <ul class="contects-info">
 
-                 <li><i class="fa fa-phone"></i>033333544</li>
-                 <li> <a href="#"><i class="fa fa-envelope"></i>Teamalpha159k@gmail.com</a></li>
-                 <li><i class="fa fa-hourglass"></i>9:00 AM to 11:00 AM</li>
+             <div>
+                 <ul class="contects-info">
 
-             </ul>
+                     <li><i class="fa fa-phone"></i>033333544</li>
+                     <li> <a href="#"><i class="fa fa-envelope"></i>Teamalpha159k@gmail.com</a></li>
+                     <li><i class="fa fa-hourglass"></i>9:00 AM to 11:00 AM</li>
 
+                 </ul>
+             </div>
              <div class="social-icons">
                  <ul>
                      <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
@@ -29,5 +31,22 @@
                  </ul>
              </div>
 
+         </div>
+
+         <div class="mid-section">
+             <div class="logo">
+                 <img src="imgs/logo.png" alt="">
+             </div>
+             <div class="basic-info-section">
+                 <div class="contect-info">
+                     <i class="fa fa-phone"></i>
+                 </div>
+                 <div class="msg-info">
+                     <i class="fa fa-envelope"></i>
+                 </div>
+                 <div class="location-info">
+                     <i class="fas fa-map-marker-alt"></i>
+                 </div>
+             </div>
          </div>
      </div>
